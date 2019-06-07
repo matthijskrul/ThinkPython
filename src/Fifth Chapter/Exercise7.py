3dd3c6ac45b1c557c17fa2655b355a5c6049e981
Matthijs Krul <matthijs.a.krul@gmail.com>
2019-06-08 00:16:00 +0200

    Refactored Exercise 7 of Chapter 5

--- src/Fifth Chapter/Exercise7.py
+++ src/Fifth Chapter/Exercise7.py
@@ -1,14 +1,20 @@
 # Extend Exercise 6 so that the sides can be given to the function in any order.
 
 
-def is_rightangled():
-    triangle_side_a = float(input("What is the length of the first side? "))
-    triangle_side_b = float(input("What is the length of the second side? "))
-    triangle_side_c = float(input("What is the length of the third side? "))
-    if triangle_side_c == ((triangle_side_a**2) + (triangle_side_b**2))**0.5 or triangle_side_a == ((triangle_side_c**2) + (triangle_side_b**2))**0.5 or triangle_side_b == ((triangle_side_c**2) + (triangle_side_a**2))**0.5:
-        print(True)
-    else:
-        print(False)
+def is_rightangled(a, b, c):
+    return a == ((b**2)+c**2)**0.5
 
 
-is_rightangled()
+triangle_side_a = float(input("What is the length of the first side? "))
+triangle_side_b = float(input("What is the length of the second side? "))
+triangle_side_c = float(input("What is the length of the third side? "))
+
+if (is_rightangled(triangle_side_a, triangle_side_b, triangle_side_c)
+    or is_rightangled(triangle_side_a, triangle_side_c, triangle_side_b)
+    or is_rightangled(triangle_side_b, triangle_side_a, triangle_side_c)
+    or is_rightangled(triangle_side_b, triangle_side_c, triangle_side_a)
+    or is_rightangled(triangle_side_c, triangle_side_a, triangle_side_b)
+    or is_rightangled(triangle_side_c, triangle_side_b, triangle_side_a)):
+    print(True)
+else:
+    print(False)
